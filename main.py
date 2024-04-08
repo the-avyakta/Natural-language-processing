e215b90ec40bbffa184b1925b2370bec89664724
Aniket Mishra <info@bizoally.com>
2024-04-08 20:22:57 +0530

    lemmatizer and stemming

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import nltk 
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from keras.preprocessing.text import text_to_word_sequence
 
 
-nltk.download('stopwords')
+nltk.download('wordnet')
 
-Sentence = '''It's important to note that "The Rudest Book Ever" has generated controversy for its blunt language and unconventional advice. While some readers appreciate its straightforward approach, others may find it offensive or inappropriate. As with any self-help book, it's essential to approach the content critically and consider how it aligns with your values and beliefs.'''
-
+Sentence = '''Natural Language Processing (NLP) is a field of artificial intelligence (AI) that focuses on the interaction between computers and humans through natural language. It encompasses a variety of tasks such as text analysis, machine translation, sentiment analysis, and speech recognition. NLP techniques enable computers to understand, interpret, and generate human language in a way that is both meaningful and useful. With the exponential growth of textual data on the internet, NLP has become increasingly important in applications ranging from chatbots and virtual assistants to content recommendation systems and information extraction. Despite its challenges, NLP continues to advance rapidly, driven by innovations in deep learning, neural networks, and computational linguistics'''
 tokens= word_tokenize(Sentence)
 
 token_words = [word for word in tokens if word.isalnum() and word.lower() not in stopwords.words('english')]
 
 clean_sentence = ' '.join(token_words)
+stemmer = PorterStemmer()
+stem_words= [stemmer.stem(word) for word in tokens]
+
+lemmatizer = WordNetLemmatizer()
+lemma_words= [lemmatizer.lemmatize(word) for word in tokens]
+print('\n'*5)
+print("stem words", stem_words)
+print('\n'*5)
+
+print(" lemma_words", lemma_words)
