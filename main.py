46e68d2942165c09c7f46087dfae0beed4cf4fe7
Aniket Mishra <info@bizoally.com>
2024-04-08 17:34:11 +0530

    tokenization

---
+++ main.py
@@ -0,0 +1,12 @@
+import nltk 
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from keras.preprocessing.text import text_to_word_sequence
+
+
+Sentence = '''It's important to note that "The Rudest Book Ever" has generated controversy for its blunt language and unconventional advice. While some readers appreciate its straightforward approach, others may find it offensive or inappropriate. As with any self-help book, it's essential to approach the content critically and consider how it aligns with your values and beliefs.'''
+
+tokens= word_tokenize(Sentence)
+
+clean_sentence = [word for word in tokens if word.isalnum() and word.lower() not in stopwords.words('english')]
+
